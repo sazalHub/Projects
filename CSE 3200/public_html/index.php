52a42481bcf9e452e9c314755718da999317dd37
Sazal Kumar Sarkar <sazalsarkar24@gmail.com>
2018-07-06 16:53:46 +0600

    Update index.php

--- CSE 3200/public_html/index.php
+++ CSE 3200/public_html/index.php
@@ -3,7 +3,7 @@
 	session_start();
 	
 	
-	$connection = mysqli_connect('localhost', 'id6300094_alumni','alumni','id6300094_alumni');
+	$connection = mysqli_connect('localhost', 'userName','Password','databaseName') or die(mysqli_error());
 ?>
 
 <!doctype html>
