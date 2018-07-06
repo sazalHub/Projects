25f6141b3a511f9ba417931326a835b6bf8a3118
Sazal Kumar Sarkar <sazalsarkar24@gmail.com>
2018-07-06 16:46:26 +0600

    Update addImages.php

--- CSE 3200/public_html/addImages.php
+++ CSE 3200/public_html/addImages.php
@@ -4,7 +4,7 @@
 	$roll = $_SESSION['user'];
 	$album_name = $_GET['album_name'];
 	
-	$connection = mysqli_connect('localhost', 'id6300094_alumni','alumni','id6300094_alumni') or die(mysqli_error());
+	$connection = mysqli_connect('localhost', 'userName','Password','databaseName') or die(mysqli_error());
 
 ?>
 
