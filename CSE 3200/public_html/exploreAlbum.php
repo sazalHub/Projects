00c923840dc53bd9963b4ccb9288b5cd0ad295ee
Sazal Kumar Sarkar <sazalsarkar24@gmail.com>
2018-07-06 16:52:36 +0600

    Update exploreAlbum.php

--- CSE 3200/public_html/exploreAlbum.php
+++ CSE 3200/public_html/exploreAlbum.php
@@ -1,7 +1,7 @@
 <?php
 	
 	session_start();
-	$connection = mysqli_connect('localhost', 'id6300094_alumni','alumni','id6300094_alumni') or die(mysqli_error());
+	$connection = mysqli_connect('localhost', 'userName','Password','databaseName') or die(mysqli_error());
 	$album_name = $_GET['album_name'];
 
 ?>
