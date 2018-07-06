7b9fc0691f60f081e2d594f27b7c62b8dd000608
Sazal Kumar Sarkar <sazalsarkar24@gmail.com>
2018-07-06 16:40:38 +0600

    Update Add_Advice.php

--- CSE 3200/public_html/Add_Advice.php
+++ CSE 3200/public_html/Add_Advice.php
@@ -4,7 +4,7 @@
     $roll = $_SESSION['user'];
     $added_by = $_GET['added_by'];
 
-	$connection = mysqli_connect('localhost', 'id6300094_alumni','alumni','id6300094_alumni') or die(mysqli_error());
+	$connection = mysqli_connect('localhost', 'userName','Password','databaseName') or die(mysqli_error());
 
 	if (isset($_POST['add_advice'])){
         
